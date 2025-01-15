3fe81b863c82c1cd28b20a2c429d1798c7794733
DigitalMete <digitalmentedm@gmail.com>
2025-01-15 18:11:57 -0500

    Added functionality to load and display cart products from localStorage.

--- js/main.js
+++ js/main.js
@@ -245,7 +245,17 @@ function actualizarBotonesAgregar() {
     });
 }
 
-const productosEnCarrito = [];
+let productosEnCarrito;
+
+const productosEnCarritoLS = JSON.parse(localStorage.getItem("productos-en-carrito"));
+
+if (productosEnCarritoLS) {
+    productosEnCarrito = productosEnCarritoLS;
+    actualizarNumerito();
+} else {
+    productosEnCarrito = [];
+}
+
 
 function agregarAlCarrito(e) {
 
@@ -256,20 +266,21 @@ function agregarAlCarrito(e) {
         const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
         productosEnCarrito[index].cantidad++;
 
-    } else {
-    productoAgregado.cantidad = 1;
-    productosEnCarrito.push(productoAgregado);
+    }  else {
+        productoAgregado.cantidad = 1;
+        productosEnCarrito.push(productoAgregado);
     }
   
-    actualizarNumerito(); 
-    localStorage.setItem("Productos-en-carrito", JSON.stringify(productosEnCarrito));
+    actualizarNumerito();
+    
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
 
 }
  
 function actualizarNumerito() {
     let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
     numerito.innerText = nuevoNumerito;
-     
+
 }
 
 
